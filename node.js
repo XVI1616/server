675a424cbb047240fbae4f47ec58b83fa0f7c996
XVI1616 <sashasabugasov21@gmail.com>
2024-07-29 01:17:50 +0300

    Update node.js

--- node.js
+++ node.js
@@ -16,14 +16,14 @@ function sendEmail(ip) {
   const transporter = nodemailer.createTransport({
     service: "yandex",
     auth: {
-      user: 'zqwe821@yandex.ru',
-      pass: 'Sasha21102007',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: 'zqwe821@yandex.ru',
-    to: 'zqwe821@yandex.ru',
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
     subject: "New visitor on my site)",
     text: `Ip adress: ${ip}`,
   };
